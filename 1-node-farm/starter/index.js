a693d69d6ad0536aa18169e4104af44d9a54027b
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-01 14:24:20 +0700

    Finished item 11. Creating a Simple Web Server

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+const http = require('http');
 
+
+///// FILE /////
 /**
  * Blocking, synchronous way
  */
@@ -16,20 +19,31 @@ console.log('File written');
 
 /**
  * Non-blocking, asynchronous way
- */
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//  */
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 
-    if (err) return console.log('Error! 🔆');
+//     if (err) return console.log('Error! 🔆');
 
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written 😘');
-            });
-        });
-    });
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written 😘');
+//             });
+//         });
+//     });
+// });
+//console.log('Will read file!');
+
+
+///// SRTVER /////
+const server = http.createServer((req, res) => {
+    //console.log(req);
+    res.end('Hello from the server!');
 });
-console.log('Will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
