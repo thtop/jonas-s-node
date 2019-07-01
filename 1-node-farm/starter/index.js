bbdfe77a17c770fe9e4f8621ebd6a7cf59d128d2
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-01 15:58:27 +0700

    Finished item 13 Building a (Very) Simple API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -40,14 +40,28 @@ console.log('File written');
 
 
 ///// SRTVER /////
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     
     const pathName = req.url;
 
     if (pathName === '/' || pathName === '/overview') {
+
         res.end('This is the OVERVIEW');
+
     } else if (pathName === '/product') {
+
         res.end('This is the PRODUCT');
+
+    } else if (pathName === '/api') {
+        
+        res.writeHead(200, { 'Content-type': 'application/json'});
+        res.end(data);
+
     } else {
         res.writeHead(404, {
             'Contnet-type': 'text/html',
@@ -55,8 +69,6 @@ const server = http.createServer((req, res) => {
         });
         res.end('<h1>Page not found!</h1>');
     }
-
-    res.end('Hello from the server!');
 });
 
 server.listen(8000, '127.0.0.1', () => {
