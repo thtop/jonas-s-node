92f65a0b329c4edaab7984e6130a3ee6c8948a7a
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-08 17:27:35 +0700

    Finished item 57. Refactoring Our Routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,20 +5,9 @@ const app = express();
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.status(404).json({
-//         message: 'Hello from the server side!',
-//         app: 'Natours'
-//     });
-// });
-
-// app.post('/', (req, res) => {
-//     res.send('You can post to this endpoint...');
-// });
-
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -26,9 +15,9 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
     //console.log(req.body);
 
     const newId = tours[tours.length -1].id + 1;
@@ -44,11 +33,9 @@ app.post('/api/v1/tours', (req, res) => {
             }
         }); 
     });
+};
 
-    //res.send('Done');
-});
-                    // :id/:x/:y?
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
     console.log(req.params);    
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
@@ -67,9 +54,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
             tour
         }
     });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
 
     if (req.params.id * 1 > tours.length) {
         return res.status(400).json({
@@ -83,9 +70,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
             tour: '<Updated tour here...>'
         }
     });
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
 
     if (req.params.id * 1 > tours.length) {
         return res.status(400).json({
@@ -97,9 +84,16 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     res.status(204).json({
         data: null
     });
-});
+};
 
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour); 
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
