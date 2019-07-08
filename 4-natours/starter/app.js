71638055c51eeb09a9618484be767c321b6f9f39
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-08 16:41:58 +0700

    Finished item 54, Responding to URL Parameters

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -47,6 +47,29 @@ app.post('/api/v1/tours', (req, res) => {
 
     //res.send('Done');
 });
+                    // :id/:x/:y?
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);    
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    //if (id > tours.length) {
+    if (!tour) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+});
+
 
 const port = 3000;
 app.listen(port, () => {
