14422bb171e0dec4957fab98036d3f92dc84912a
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-09 04:03:41 +0700

    Finished item 59. Creating Our Own Middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,14 +2,26 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware 👋');
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 const getAllTours = (req, res) => {
+    console.log(req.requestTime);
+
     res.status(200).json({
         status: 'success',
+        requestedAt: req.requestTime,
         results: tours.length,
         data: {
             tours
@@ -20,8 +32,10 @@ const getAllTours = (req, res) => {
 const createTour = (req, res) => {
     //console.log(req.body);
 
-    const newId = tours[tours.length -1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({
+        id: newId
+    }, req.body);
 
     tours.push(newTour);
 
@@ -31,12 +45,12 @@ const createTour = (req, res) => {
             data: {
                 tour: newTour
             }
-        }); 
+        });
     });
 };
 
 const getTour = (req, res) => {
-    console.log(req.params);    
+    console.log(req.params);
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
 
