e6f16fa79d4869deeb05927dcdfc006af644bf43
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-10 11:15:03 +0700

    Finished item 84. Creating a Simple Tour Model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -6,7 +6,10 @@ dotenv.config({
   path: './config.env'
 });
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 // LOCAL
 // mongoose.connect(process.env.DATABASE_LOCAL, {
@@ -16,15 +19,31 @@ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSW
 // }).then(() => console.log('DB connection successful!'));
 
 // HOST
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(() => console.log('DB connection successful!'));
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
 
-//console.log(app.get('env'));
-//console.log(process.env);
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
