461ffcd2873b9e7a139ed3fe666ba18feeae5e92
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-11 16:08:17 +0700

    Finished item 94. Making the API Better: Filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,7 +2,26 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find({});
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    const query = await Tour.find(queryObj);
+
+    // const query = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy'
+    // });
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
