e3812f96ea5ffbe291f49ad190bfa7cc2fa37b96
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-11 15:09:36 +0700

    Finished item 93. Importing Development Data

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -2,23 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({
-  path: './config.env'
-});
+dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-// LOCAL
-// mongoose.connect(process.env.DATABASE_LOCAL, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false
-// }).then(() => console.log('DB connection successful!'));
-
-// HOST
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
