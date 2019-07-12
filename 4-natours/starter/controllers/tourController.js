96e6f236f06e930c030d5bada2988370ce9ac97b
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-12 11:36:46 +0700

    Deleted comments

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -13,16 +13,13 @@ exports.getAllTours = async (req, res) => {
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    //console.log(JSON.parse(queryStr));
 
     let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      //console.log(sortBy);
       query = query.sort(sortBy);
-      // sort('price ratingsAverage')
     } else {
       query = query.sort('-createdAt');
     }
