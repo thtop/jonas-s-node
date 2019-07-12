f42dcbed9a8f8bb34241860003a3542722e8d15a
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-12 11:32:49 +0700

    Finished item 96. Making the API Better: Sorting

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,22 +2,30 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    // 1) BUILD QUERY
+    // BUILD QUERY
+    // 1A) Filtering
     const queryObj = {
       ...req.query
     };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // 2) Advanced filtering
+    // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    //console.log(JSON.parse(queryStr));
 
-    // { difficulty: 'easy', duration: {$gte: 5} }
-    // { difficulty: 'easy', duration: { gte: '5' } }
-    // gte, gt, lte, lt
+    let query = Tour.find(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      //console.log(sortBy);
+      query = query.sort(sortBy);
+      // sort('price ratingsAverage')
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
