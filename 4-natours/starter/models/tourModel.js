8e2c4f834ba25fc3a486603b968b19ff6d9c623f
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-22 21:02:13 +0700

    Finished item 106. Aggregation Middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -98,7 +98,15 @@ tourSchema.pre(/^find/, function(next) {
 
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
+  // console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift();
+  
+  console.log(this.pipeline());
   next();
 });
 
